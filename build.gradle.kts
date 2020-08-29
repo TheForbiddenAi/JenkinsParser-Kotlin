0cefa8fe5fd10e85a25f2ff6b8f2ed17f33b0e02
TheForbiddenAi <theforbiddenai@gmail.com>
2020-08-29 18:14:03 -0400

    Migrated Gradle's DSL to Gradle's Kotlin DSL Updated jsoup to version 13 Kotlin updated to version 1.4.0
    Signed-off-by: TheForbiddenAi <theforbiddenai@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.0"
+}
+
+group = "me.theforbiddenai"
+version = "1.3.2"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("org.jsoup:jsoup:1.13.1")
+}
